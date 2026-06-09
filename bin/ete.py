0930679d6ec8b5f3bdf08649cdca0564330e7e28
jissksaji <jissksaji@gmail.com>
2026-06-09 09:08:34 +0200

    updated ete

--- bin/ete.py
+++ bin/ete.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#ete.py
-#This script does tree traversal.
-# traverses from family to species.
-# any node in the tree not present in the blast db is set as not present
-# pass means present in the blast db and found
-# fail means present in the blast db and not found
-# no_data means not present in the blast db
-#the tree use ete to get the current no of entries in each rank 
-
 import argparse
 from ete3 import NCBITaxa
 import re
@@ -41,114 +32,55 @@ def main(taxname, refs, report, output):
     r = open(report, "r")
     lines = r.readlines()
     bucket = {}
+    lines.pop()  # remove the header
 
     for line in lines:
-        line = line.strip()
-        if not line:
-            continue
         elements = line.split("\t")
-        if len(elements) >= 5:  
-            sci = elements[-4]
-            tax = elements[-3]
-            bucket[tax] = sci
+        sci = elements[1]
+        tax = elements[2]
+        bucket[tax] = sci
 
     r.close()
 
-    # Expand bucket keys downward to ensure species under a genus-level LCA inherit the OK status
-    expanded_bucket = set(bucket.keys())
-    for tax in list(bucket.keys()):
-        try:
-            descendants = ncbi.get_descendant_taxa(tax, collapse_subspecies=False)
-            expanded_bucket.update(map(str, descendants))
-        except ValueError:
-            pass
-
     # the list taxids included in the BlastDB
     with open(refs, "r") as taxids:
-        blast_tax = set([tax.strip() for tax in taxids])
-
-    # Intersect with blast_tax to prevent expanding into taxa that aren't even in the DB,
-    # while keeping the original bucket keys that were explicitly in the report.
-    expanded_bucket = expanded_bucket.intersection(blast_tax).union(bucket.keys())
+        blast_tax = taxids.readlines()
+        blast_tax = [tax.rstrip('\n') for tax in blast_tax]
 
     ok = "#7ee076"
     fail = "#dfc2b1"
     missing = "#eeeeee"
     tree = ncbi.get_descendant_taxa(taxid, collapse_subspecies=False, return_tree=True)
-    species_names = [
-        n.sci_name for n in tree.traverse()
-        if n.rank == "species" and re.match(r"^[A-Z][a-z]*\s[a-z]*$", n.sci_name)
-    ]
-    species_count = len(species_names)
-    print(f"Total species in tree: {species_count}", flush=True)
-    with open(output + ".species_count.txt", "w") as sc:
-        sc.write(f"root_taxon\t{taxname}\n")
-        sc.write(f"total_species_in_tree\t{species_count}\n")
-        for name in sorted(species_names):
-            sc.write(f"{name}\n")
-            
-    # Calculate Family Coverage before modifying tree nodes
-    family_data = {}
-    for n in tree.traverse():
-        if n.rank == "family":
-            family_tid = str(n.name)
-            descendant_tids = [str(desc.name) for desc in n.traverse()]
-            if any(tid in expanded_bucket for tid in descendant_tids):
-                family_data[n.sci_name] = f"OK\t{family_tid}\t{ok}\tfamily"
-            elif any(tid in blast_tax for tid in descendant_tids):
-                family_data[n.sci_name] = f"FAIL\t{family_tid}\t{fail}\tfamily"
-            else:
-                family_data[n.sci_name] = f"NO_DATA\t{family_tid}\t{missing}\tfamily"
-
-    # Calculate Genus Coverage
-    genus_data = {}
-    for n in tree.traverse():
-        if n.rank == "genus":
-            genus_tid = str(n.name)
-            descendant_tids = [str(desc.name) for desc in n.traverse()]
-            if any(tid in expanded_bucket for tid in descendant_tids):
-                genus_data[n.sci_name] = f"OK\t{genus_tid}\t{ok}\tgenus"
-            elif any(tid in blast_tax for tid in descendant_tids):
-                genus_data[n.sci_name] = f"FAIL\t{genus_tid}\t{fail}\tgenus"
-            else:
-                genus_data[n.sci_name] = f"NO_DATA\t{genus_tid}\t{missing}\tgenus"
-
     data = {}
 
     nodes = []
     for n in tree.traverse():
-        if n.rank == "species":
-            tid = str(n.name)
-
-            # NCBI taxonomy is full of non-species level terminal leafs
-            # we skip all leafs not matching the 'Genus species' pattern
-            if re.match(r"^[A-Z][a-z]*\s[a-z]*$", n.sci_name):
-                descendant_tids = [str(desc.name) for desc in n.traverse()]
-                # This taxon is in the blast db and was found
-                if any(t in expanded_bucket for t in descendant_tids):
-                    data[n.sci_name] = f"OK\t{tid}\t{ok}\tspecies"
-                # This taxon is in the blast db and was not found
-                elif any(t in blast_tax for t in descendant_tids):
-                    data[n.sci_name] = f"FAIL\t{tid}\t{fail}\tspecies"
-                # this taxon was not in the blast db
-                else:
-                    data[n.sci_name] = f"NO_DATA\t{tid}\t{missing}\tspecies"
-                
+        if n.is_leaf:
+            # Make sure we only look at terminal nodes
+            if n.rank == "species":
+                tid = n.name
                 n.name = n.sci_name
-                nodes.append(n)
+
+                # NCBI taxonomy is full of non-species level terminal leafs
+                # we skip all leafs not matching the 'Genus species' pattern
+                if re.match(r'^[A-Z][a-z]*\s[a-z]*$', n.sci_name):
+                    # This taxon is in the blast db and was found
+                    if tid in bucket:
+                        data[n.sci_name] = f"OK\t{tid}\t{ok}"
+                    # This taxon is in the blast db and was not found
+                    elif tid in blast_tax:
+                        data[n.sci_name] = f"FAIL\t{tid}\t{fail}"
+                    # this taxon was not in the blast db
+                    else:
+                        data[n.sci_name] = f"NO_DATA\t{tid}\t{missing}"
+                    nodes.append(n)
 
     tree.prune(nodes)
 
     print(tree.write(outfile=output + ".nwk", format=1))
 
     f = open(output + ".tsv", "w")
-    f.write("Taxon\tStatus\tTaxid\tColor\tRank\n")
-    for taxon in family_data:
-        status = family_data[taxon]
-        f.write(f"{taxon}\t{status}\n")
-    for taxon in genus_data:
-        status = genus_data[taxon]
-        f.write(f"{taxon}\t{status}\n")
+    f.write("Taxon\tStatus\tTaxid\tColor\n")
     for taxon in data:
         status = data[taxon]
         f.write(f"{taxon}\t{status}\n")
@@ -156,5 +88,5 @@ def main(taxname, refs, report, output):
     f.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(args.taxon, args.reference, args.report, args.output)
